a4ce68774f91880551661fd7b609dfcca2a9e934
Milyausha Shigapova <milia.schigapowa@gmail.com>
2025-04-07 23:36:09 +0300

    Improved

--- SELECT.sql
+++ SELECT.sql
@@ -12,7 +12,7 @@ SELECT name FROM artist
 WHERE name NOT LIKE '% %';
 
 SELECT title FROM track
-WHERE title  LIKE '%my%' OR title LIKE '%мой%';
+WHERE string_to_array(lower(title), ' ') && ARRAY['my', 'мой'];
 
 SELECT g.name AS genre_name, COUNT(ga.artist_id) AS artist_count
 FROM genre g
@@ -20,11 +20,10 @@ LEFT JOIN genresartists ga ON g.id = ga.genre_id
 GROUP BY genre_name
 ORDER BY genre_name;
 
-SELECT a.title, a.release_year, COUNT(t.id) AS track_count
-FROM album a
-JOIN track t ON t.album_id = a.id
+SELECT COUNT(t.id) AS track_count
+FROM track t
+JOIN album a ON t.album_id = a.id
 WHERE a.release_year BETWEEN '2019-01-01' AND '2020-12-31'
-GROUP BY a.title, a.release_year;
 
 SELECT a.title, DATE_TRUNC('second', AVG(t.duration)) AS average_duration 
 FROM album a
@@ -32,10 +31,12 @@ JOIN track t ON t.album_id = a.id
 GROUP BY a.title;
 
 SELECT a.name AS artist_name FROM artist a
-JOIN artistsalbums aa ON a.id = aa.artist_id
-JOIN album al ON al.id = aa.album_id
-WHERE al.release_year < '2020-01-01' OR al.release_year > '2020-12-31'
-GROUP BY a.name;
+WHERE a.name NOT IN (
+    SELECT a2.name FROM artist a2
+    JOIN artistsalbums aa ON a2.id = aa.artist_id
+    JOIN album al ON al.id = aa.album_id
+    WHERE  EXTRACT(YEAR FROM al.release_year) = '2020'
+);
 
 SELECT c.name AS collection_name FROM collection c
 JOIN collectionstracks ct ON c.id = ct.collection_id
@@ -44,6 +45,8 @@ JOIN artistsalbums aa ON aa.album_id = t.album_id
 JOIN artist a ON a.id = aa.artist_id
 WHERE a.name LIKE 'Cardi B'
 GROUP BY c.name;
+
+
 
 
 
